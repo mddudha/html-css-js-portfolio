fd333cf8e57009e20db27fd192aca892e571d963
Mirva Dudhagara <middudha@iu.edu>
2025-04-12 19:47:43 -0400

    adding theme

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function loadPen(penId) {
+    const pens = {
+        pen1: "https://codepen.io/Mirva-Dudhagara/embed/dPyEqJK?default-tab=result",
+        pen2: "https://codepen.io/Mirva-Dudhagara/embed/GgRaXab?default-tab=result",
+    };
+    
+    const embedUrl = pens[penId];
+    document.getElementById("embed-area").innerHTML = `
+        <iframe height="500" style="width: 100%;" scrolling="no" title="Showcase Embed"
+            src="${embedUrl}" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+        </iframe>`;
+}
+
+
+window.onload = function() {
+    loadPen("pen1");
+};
